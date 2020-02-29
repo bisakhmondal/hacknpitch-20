dcf4d768c9f6f045c2049a0d54e2e08380aa9d19
Bisakh Mondal <bisakhmondal00@gmail.com>
2020-02-29 11:59:09 +0530

    abc

--- Frontend/bloodjs/src/App.js
+++ Frontend/bloodjs/src/App.js
@@ -3,6 +3,21 @@ import Navigation from './Components/Navigation/Navigation'
 import './App.css';
 import 'tachyons'
 class App extends React.Component {
+  constructor(){
+    super();
+    this.state={
+      isSigned:false,
+      route:''
+    }
+  }
+  onClickChangeRoute=(route)=>{
+    if(route==='home'){
+      this.setState({route:'home',isSigned:false});
+    }
+    else if(route==='signin'){
+      this.setState({route:'signin',isSigned:true});
+    }
+  }
   render(){
   return (
     <Navigation />
--- Frontend/bloodjs/src/Components/Navigation/Navigation.js
+++ Frontend/bloodjs/src/Components/Navigation/Navigation.js
@@ -5,7 +5,10 @@ export default class Navigation extends React.Component{
     render(){
       
             return(
-        
+                <nav  style={{display:'flex',justifyContent:'flex-end'}}>
+                <p onClick={()=>this.props.onRouteChange('signin')} className="pointer f3 dim black link pa3 underline">Sign In </p>
+                <p onClick={()=>this.props.onRouteChange('register')} className="pointer f3 dim black link pa3 underline">Register </p>
+                </nav>
         );
     
         
