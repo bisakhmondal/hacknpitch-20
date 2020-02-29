b3ce2436101bed28c11b68d1944af4819f5bd3b8
Bisakh Mondal <bisakhmondal00@gmail.com>
2020-02-29 11:44:55 +0530

    structure

---
+++ Frontend/bloodjs/src/Components/Navigation/Navigation.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+export default class Navigation extends React.Component{
+    render(){
+      
+            return(
+        
+        );
+    
+        
+    }
+}
+
+// onClick={()=>this.props.onRouteChange('signin')}
+
+// onClick={()=>this.props.onRouteChange('register')}
